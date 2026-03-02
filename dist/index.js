9db608834a693f5077053458e6a131292ef46e01
Pop Robert <poprobert1999@yahoo.com>
2026-03-02 17:21:39 +0200

    dist update

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let age = 25;
+age = 26;
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 let age: number = 25;
 age = 26;
 
+if (age > 18)
+    age += 50;
+else
+    age -= 50;
